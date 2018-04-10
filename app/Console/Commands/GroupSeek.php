b13575d2265859d3598c5a4eb24697f2e9d47ad9
Konstantin <goodluckhf@yandex.ru>
2018-04-10 23:49:29 +0300

    вынес базовый php образ и запустил крон отдельным сервисом

--- app/Console/Commands/GroupSeek.php
+++ app/Console/Commands/GroupSeek.php
@@ -92,7 +92,7 @@ class GroupSeek extends Command {
 	}
 	
 	private function getLinkFromWikiUrl($url) {
-		$html = HtmlDomParser::file_get_html($url);
+		$html = HtmlDomParser::file_get_html($url, false, null, 0);
 		$wikiBody = $html->find('div.wiki_body', 0);
 		if (! $wikiBody) {
 			return null;
--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,4 @@ php artisan config:cache
 php artisan key:generate
 php artisan migrate --seed
 
-crond -d 0 && php-fpm
-#echo 'root' | sudo -S service cron start
-#apache2-foreground
+php-fpm
