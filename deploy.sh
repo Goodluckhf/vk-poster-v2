dc4b77dcb5159e687165e2aa3b59c300eb506f20
Konstantin <goodluckhf@yandex.ru>
2018-04-15 00:32:52 +0300

    добавил образ docker-compose для CI

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ php composer.phar install --prefer-dist -o -vvv --no-plugins --no-scripts
 php composer.phar dump-autoload
 php artisan config:cache
 php artisan key:generate
+php artisan migrate --seed
