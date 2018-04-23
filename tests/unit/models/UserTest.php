ba830578d38fb6eb9c5a424a51078495d690f49d
Konstantin <goodluckhf@yandex.ru>
2018-04-23 20:26:08 +0300

    Тестированте модели User

--- tests/unit/models/UserTest.php
+++ tests/unit/models/UserTest.php
@@ -22,4 +22,18 @@ class UserTest extends TestCase {
 		$this->assertEquals(User::ACTIVATED, $user->role_id);
 	}
 	
+	public function testUserCanBeDeactivated() {
+		$user = factory(User::class)->make();
+		$user->activate();
+		$user->deactivate();
+		$this->assertEquals(User::USER, $user->role_id);
+	}
+	
+	public function testIsAdminReturbTrueOnlyIfUserIsAdmin() {
+		$user = factory(User::class)->make();
+		$this->assertFalse($user->isAdmin());
+		$user->role_id = USER::ADMIN;
+		$this->assertTrue($user->isAdmin());
+	}
+	
 }
