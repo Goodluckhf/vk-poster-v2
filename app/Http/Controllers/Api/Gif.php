36a2ec6599a6e89a580388b7772badbfa9c59d2b
oprevka <opr.evko@yandex.ru>
2018-04-25 20:39:17 +0300

    Прийдется еще над логикой подумать

--- app/Http/Controllers/Api/Gif.php
+++ app/Http/Controllers/Api/Gif.php
@@ -61,45 +61,8 @@ class Gif extends Api {
 			->where('user_id', $user_id)
 			->take($datesCount)
 			->get();
-		#doc<owner_id>_<doc_id>
-		$vkPostData = [];
-		foreach ($gifs as $key => $gif) {
-			$tmp = []
-			$tmp['owner_id'] 	=> Request::get('group_id') * (-1);
-			$tmp['attachments']  => 'doc'.$gif['owner_id'].'_'.$gif['doc_id'];
-			$tmp['publish_date'] => $dates[$key];
-			$tmp['from_group']   => '1';
-
-			$vkPostData.add($tmp);
-		}
-		/*
-		$vkPostData = [];
-		foreach ($gifs as $key => $gif) {
-			$tmp = []
-			$tmp['owner_id'] 	=> Request::get('group_id') * (-1);
-			$tmp['attachments']  => 'doc'.$gif['owner_id'].'_'.$gif['doc_id'];
-			$tmp['publish_date'] => $dates[$key];
-			$tmp['from_group']   => '1';
-
-			$vkPostData.add($tmp);
-		}
-
-		vkpostdata = [
-			[owner=>a, attach=>b, ...],
-			[owner=>x, attach=>y, ...],
-			...
-		]
-		callApi плюнет на такой массив
-		или нет?
-		http_build_query сделает ?0[asd]=asd&0[sasd]
-		просто их слепить?
-
-		*/
 
 		$vkApi = new VkApi($_COOKIE['vk-token']);
-		$res = $vkApi->callApi('wall.post',$vkPostsData);
-
-		/*
 		$vkPostsStr = '';
 		foreach ($gifs as $key => $gif) {
 			$vkPostsStr .= 'doc' . $gif['owner_id'] . '_'.
@@ -113,25 +76,16 @@ class Gif extends Api {
 			
 			$vkPostsStr .= ',';
 		}
-		*/
-		
+				
 		//doc173428463_459048611|message|unixDate,...
 		//но не больше 25
-		
-
-		/*
 		$res = $vkApi->callApi('execute.postGif', [
 			'owner_id' => Request::get('group_id'),
 			'posts'    => $vkPostsStr,
 			'v'        => '5.73'
 		], 'post');
-		*/
-		// 
-
-
-
-
 		
+		// 		
 		$this->_data['vkRes'] = $res['response'];
 		
 		return $this;
