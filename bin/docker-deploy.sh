9c601f6d9977fa2513c2074f92f3994cc7153f61
Konstantin <goodluckhf@yandex.ru>
2018-04-29 15:19:31 +0300

    правки

--- bin/docker-deploy.sh
+++ bin/docker-deploy.sh
@@ -7,15 +7,19 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 
 docker-compose -f compose.base.yml -f compose.dev.yml up -d --build
-sleep 3
+
+echo -e "${GREEN}Waiting 25 sec for [mysql]${NC}"
+sleep 25
 
 docker exec poster php artisan migrate --seed
+
+echo -e "${GREEN}Start install dependencies ${NC}"
+docker exec poster bash ./bin/installDeps.sh
+
 for service in "poster" "php-cron"
 do
 	echo -e "${GREEN}Start deploying [$service]${NC}"
 	docker exec $service bash ./bin/deploy.sh
-	echo -e "${GREEN}Start install dependencies [$service]${NC}"
-	docker exec $service bash ./bin/installDeps.sh
 	docker exec $service php artisan config:clear
 	echo -e "${GREEN}deploying [$service] is successfull!${NC}"
 done
